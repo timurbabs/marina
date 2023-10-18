d7e5af3a85ac3a011443322ce655f49d4b3e261d
Timur Baburin <timurbabs@Timurs-MacBook-Pro.local>
2023-10-19 02:16:06 +0300

    new cat

--- scripts.js
+++ scripts.js
@@ -1,39 +1,50 @@
 document.addEventListener('DOMContentLoaded', function () {
     var catContainer = document.getElementById('cat-container');
-    
-    // Function to generate cats
+    var catsArray = []; // Store the cat elements for overlap checks
+  
     function createCat() {
       var cat = document.createElement('img');
-      cat.src = 'images/photo2.png'; // Replace with your cat image path
+      cat.src = 'path-to-your-cat-image.png'; // Replace with your cat image path
       cat.classList.add('cat');
   
-      // Random position
-      cat.style.left = Math.random() * 100 + 'vw';
-      cat.style.top = Math.random() * 100 + 'vh';
+      // Suggesting fixed dimensions for calculation, can be made dynamic
+      var catWidth = 100;
+      var catHeight = 150;
   
-      // Random animation duration
+      var left, top, isOverlapping;
+  
+      // Prevent overlapping cats with a simple check (can be more sophisticated)
+      do {
+        left = Math.random() * (window.innerWidth - catWidth);
+        top = Math.random() * (window.innerHeight - catHeight);
+        isOverlapping = catsArray.some(function(existingCat) {
+          var rect = existingCat.getBoundingClientRect();
+          return !(rect.right < left || rect.left > left + catWidth || rect.bottom < top || rect.top > top + catHeight);
+        });
+      } while (isOverlapping);
+  
+      cat.style.left = left + 'px';
+      cat.style.top = top + 'px';
       cat.style.animationDuration = (Math.random() * 3 + 2) + 's';
   
-      // Click event: apply falling animation
       cat.addEventListener('click', function() {
-        cat.style.animationName = 'fall';
-        cat.style.animationDuration = '2s';
-        cat.style.animationTimingFunction = 'linear';
-        cat.style.animationFillMode = 'forwards'; // Keep the end state
+        cat.style.animation = 'fall 2s forwards linear'; // Updated to new smoother animation
   
-        // Remove the cat after falling
         setTimeout(function() {
           catContainer.removeChild(cat);
-        }, 2000); // Should match the duration of the falling animation
+          catsArray = catsArray.filter(function(existingCat) {
+            return existingCat !== cat; // Remove reference from the array
+          });
+        }, 2000); // Ensure this matches the animation duration
       });
   
       catContainer.appendChild(cat);
+      catsArray.push(cat); // Add the new cat to our tracking array
     }
   
-    // Calculate the number of cats based on screen size (example)
+    // Number of cats based on viewport or fixed number, adjust as needed
     var numberOfCats = Math.ceil(window.innerWidth / 150) * Math.ceil(window.innerHeight / 150);
-    
-    // Create as many cats as calculated
+  
     for (var i = 0; i < numberOfCats; i++) {
       createCat();
     }
